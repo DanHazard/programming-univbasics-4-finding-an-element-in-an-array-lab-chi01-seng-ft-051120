0ceba3eb7af62049c5834fdb787daa92cd0ce8f8
Dan Love <daniel.love.jr@gmail.com>
2020-04-28 20:59:04 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,8 +1,11 @@
+require 'pry'
+
 def find_element_index(array, value_to_find)
   # Add your solution here
   array.length.times do |index|
     if array[index] == value_to_find
       return array.index[index]
+      binding.pry
     else
       return nil
     end

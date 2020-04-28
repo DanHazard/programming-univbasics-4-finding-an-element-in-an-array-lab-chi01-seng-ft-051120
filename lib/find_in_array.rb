b769d1e5d1f8a11a1e4868a50a33a792d2815f1e
Dan Love <daniel.love.jr@gmail.com>
2020-04-28 21:09:55 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -6,6 +6,8 @@ def find_element_index(array, value_to_find)
     if array.include?(value_to_find)
       return array.index(value_to_find)
       binding.pry
+    else
+      return nil
     end
   end
 end

e4d9a327f929831145486612cbcb51ea8c9b3ac3
Dan Love <daniel.love.jr@gmail.com>
2020-04-28 21:04:16 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -5,8 +5,6 @@ def find_element_index(array, value_to_find)
   array.length.times do |index|
     if array[index] == value_to_find
       array.index(value_to_find)
-    else
-      return nil
     end
   end
 end
